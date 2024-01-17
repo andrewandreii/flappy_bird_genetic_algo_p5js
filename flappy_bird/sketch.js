1609b01295fb1cdc2abdf5d2029ea9bc16890cb5
TableFlipper9 <hithack9@gmail.com>
2024-01-17 22:08:01 +0200

    Update sketch.js

--- flappy_bird/sketch.js
+++ flappy_bird/sketch.js
@@ -41,8 +41,8 @@ function getResource(filename) {
 function preload() {
   pipeBodySprite = getResource('gate.png');
   pipePeakSprite = getResource('gate.png');
-  Bird.default_sprite = getResource('bird_up.png');
-  Bird.flip_sprite = getResource('bird_down.png');
+  Bird.default_sprite = getResource('bird_down.png');
+  Bird.flip_sprite = getResource('bird_up.png');
   Pipe.spacing = maxGap;
   bgImg = getResource('background.png');
   pipeFrequancy = 0;
